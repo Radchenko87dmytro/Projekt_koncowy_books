6312d56b003c94d2fdf6ab1a2ae01dcba2684095
Radchenko87dmytro <94636314+Radchenko87dmytro@users.noreply.github.com>
2023-07-21 00:41:21 +0200

    Revert "Some mergin conflict resolving (revert)"
    This reverts commit bd6d6d26e5207db5bc672553af31f21042e0ed28.

--- src/App.js
+++ src/App.js
@@ -1,87 +1,95 @@
-//import logo from './logo.svg';
 import './App.scss';
 import AllBooks from './components/ALLBooks/AllBooks';
-import Button from './components/Button';
-//import FundamentalCours from './components/fundamentalCoutse';
-import Edit_book from './components/Edit_book';
+import AllBooksClass from './components/ALLBooks/AllBooksClass';
 import Header from './components/Header/Header';
 import { useState } from 'react';
-import OneBook from './components/ALLBooks/OneBook';
-
+import axios from "axios";
 
 const state = [
     {
-    id: 0,
+    id: 1,
     headLine: "W pustyni i puszczy",
     author: "Juliusz Słowacki",
     opis: "Ksiązka długa, fajna i nudna jedocześnie. ",
   },
   {
-    id: 1,
+    id: 2,
     headLine: "Zniewolony umysł",
     author: "Czesław Miłosz",
     opis: "Ksiązka fajna",
   },
   {
-    id: 2,
+    id: 3,
     headLine: "Dwukropek",
     author: "Wisława Szymborska",
     opis: "Nie nudna ksiązka",
   },
   {
-    id: 3,
+    id: 4,
     headLine: "JavaScript",
     author: "Sochacki Tomasz",
     opis: "Nauka się wykorzystać go w praktyce",
   },
 ]
 
-
 const App = ()=> {
-  
+
   const[stateValue, setNewBookValue] = useState(state)
+  
+  const addBookHandler = (inputValue)=>{
+    inputValue.id=stateValue.length + 1
+    setNewBookValue([...stateValue, inputValue])
+  }
+
 
   const onChange = (sortValue) =>{
-    console.log(sortValue)
-    setNewBookValue([...stateValue].sort((a, b)=>a[sortValue].localeCompare(b[sortValue])))
+    setNewBookValue([...stateValue].sort((a, b)=>a[sortValue].localeCompare(b[sortValue])))  //.sort() method sorts the elements of an array   The localeCompare() method compares two strings in the current locale
     //setNewBookValue(currentItems.sort((a, b)=>a.sortValue < b.sortValue ? 1 : -1))
-  } 
+  }
+  
+  const searchFormHandler = (searchQuery)=>{
+    !searchQuery 
+    ?
+    setNewBookValue([...stateValue])
+    :
+    setNewBookValue([...stateValue].filter(item => item.headLine.toLowerCase().includes(searchQuery.toLowerCase())))                   
+  }
 
-     const addBookHandler = (inputValue)=>{
-        setNewBookValue([...stateValue, inputValue])
-    }
+  
 
   const deleteBook = (pos) =>{
-
     if (window.confirm("Are you sure delete Book?")){ //modal window
-   
-     console.log(pos)
     const temp = [...stateValue]
     temp.splice(pos, 1)
     setNewBookValue(temp)
-
     //setNewBookValue(stateValue.filter(item => item.id !== pos))
     }
   } 
  
-  const editBook= (inputValue, index)=>{
-    
-    console.log("edit")
-    console.log(inputValue)
-    
+  const editBook= (inputValue)=>{
      const allBooks = [...stateValue]
      allBooks[inputValue.id] = inputValue
-     console.log(allBooks[inputValue.id])
      setNewBookValue(allBooks)
     }
   
   return (
     <div className="App">
       <Header addBookHandler={addBookHandler} />
-      <AllBooks  stateValue={stateValue} deleteBook={deleteBook} editBook={editBook} onChange={onChange}/>
-      
-      {/* <AllBooks addBookHandler={addBookHandler}/> */}
+      <hr></hr>
 
+      <AllBooks stateValue={stateValue} 
+                deleteBook={deleteBook} 
+                editBook={editBook} 
+                onChange={onChange} 
+                searchFormHandler={searchFormHandler}/>
+      <hr></hr>
+      
+      <AllBooksClass  
+                deleteBook={deleteBook} 
+                editBook={editBook} 
+                onChange={onChange} 
+                searchFormHandler={searchFormHandler}/>
+                
     </div>
   )
 }
